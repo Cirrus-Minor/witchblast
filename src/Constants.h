e4af1ee2b249fe792668fe564bcfece4c7e0f6ba
Seby <sebygames@gmail.com>
2014-07-07 20:12:00 +0200

    Update version

--- src/Constants.h
+++ src/Constants.h
@@ -23,7 +23,7 @@
 #include <string>
 
 const std::string APP_NAME =      "Witch Blast";
-const std::string APP_VERSION =   "0.1.5";
+const std::string APP_VERSION =   "0.2.0";
 
 const std::string CONFIG_FILE =   "config.dat";
 const std::string SAVE_FILE =     "game.sav";
