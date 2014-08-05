bc6463e6653d558c5bd918f9ca2943d92ffe4972
Seby <sebygames@gmail.com>
2014-08-05 17:39:32 +0200

    FIX Evil Flower hat false type

--- src/EvilFlowerEntity.cpp
+++ src/EvilFlowerEntity.cpp
@@ -18,7 +18,7 @@ EvilFlowerEntity::EvilFlowerEntity(float x, float y)
   setSpin(50.0f);
   frame = 0;
 
-  type = 23;
+  type = ENTITY_ENNEMY;
   bloodColor = bloodGreen;
   enemyType = EnemyTypeEvilFlower;
 
