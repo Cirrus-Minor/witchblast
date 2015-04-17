5e46d453a8df4027e3df728da03127d16844255b
Seby <sebygames@gmail.com>
2015-04-17 10:09:43 +0200

    MOD Better drop for standard chests when lucky

--- src/ChestEntity.cpp
+++ src/ChestEntity.cpp
@@ -165,7 +165,7 @@ void ChestEntity::open()
     {
       // gold
       int r = 2 + rand() % 6;
-      if (game().getPlayer()->isEquiped(EQUIP_LUCK)) r += rand() % 6;
+      if (game().getPlayer()->isEquiped(EQUIP_LUCK)) r += 1 + rand() % 5;
       for (int i = 0; i < r; i++)
       {
         ItemEntity* newItem = new ItemEntity(ItemCopperCoin, x, y);
