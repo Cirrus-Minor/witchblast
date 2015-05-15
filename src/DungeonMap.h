bc698a11176dd59124365ed10eeeeb4fc90497e5
Seby <sebygames@gmail.com>
2015-05-15 09:36:00 +0200

    MOD Add one random tile / room
    ADD Random tile rarity

--- src/DungeonMap.h
+++ src/DungeonMap.h
@@ -104,37 +104,38 @@ struct randomDungeonTileStruct
   bool canRotate;
   bool canBeOnWall;
   bool randomPlace;
+  int rarity;
 };
 
 const int NB_RANDOM_TILES = 21;
 const int NB_RANDOM_TILE_TRY = 40;
-const int NB_RANDOM_TILES_IN_ROOM = 3;
+const int NB_RANDOM_TILES_IN_ROOM = 4;
 
 /** Array with all the items and data */
 const randomDungeonTileStruct randomDungeonTiles[NB_RANDOM_TILES] =
 {
-  { 192, 192, 0, 0, true, true, true },
-  { 192, 192, 192, 0, true, true, true },
-  { 192, 192, 384, 0, true, true, true },
-  { 192, 192, 576, 0, true, true, true },
-  { 256, 256, 0, 192, true, true, true },
-  { 512, 256, 256, 192, true, true, true },
-  { 128, 128, 0, 448, true, false, true },
-  { 128, 128, 128, 448, true, false, true },
-  { 128, 128, 256, 448, true, false, true },
-  { 128, 128, 384, 448, false, false, false },
-  { 128, 128, 512, 448, false, false, false},
-  { 128, 128, 640, 448, false, false, false },
-  { 192, 192, 0, 576, true, true, true },
-  { 192, 192, 192, 576, true, true, true },
-  { 192, 192, 384, 576, true, true, true },
-  { 192, 192, 576, 576, true, true, true },
+  { 192, 192, 0, 0, true, true, true, 1},
+  { 192, 192, 192, 0, true, true, true, 1},
+  { 192, 192, 384, 0, true, true, true, 1},
+  { 192, 192, 576, 0, true, true, true, 1},
+  { 256, 256, 0, 192, true, true, true, 1},
+  { 512, 256, 256, 192, true, true, true, 1},
+  { 128, 128, 0, 448, true, false, true, 6},
+  { 128, 128, 128, 448, true, false, true, 3},
+  { 128, 128, 256, 448, true, false, true, 3},
+  { 128, 128, 384, 448, false, false, false, 1},
+  { 128, 128, 512, 448, false, false, false, 6},
+  { 128, 128, 640, 448, false, false, false, 6},
+  { 192, 192, 0, 576, true, true, true, 1},
+  { 192, 192, 192, 576, true, true, true, 1},
+  { 192, 192, 384, 576, true, true, true , 1},
+  { 192, 192, 576, 576, true, true, true, 1},
 
-  { 256, 256, 0, 768, true, true, true },
-  { 256, 256, 256, 768, true, true, true },
-  { 256, 192, 768, 0, true, true, true },
-  { 256, 192, 768, 192, true, true, true },
-  { 256, 192, 768, 384, true, true, true },
+  { 256, 256, 0, 768, true, true, true, 1},
+  { 256, 256, 256, 768, true, true, true, 1},
+  { 256, 192, 768, 0, true, true, true, 1},
+  { 256, 192, 768, 192, true, true, true, 1},
+  { 256, 192, 768, 384, true, true, true, 1},
 };
 
 class DungeonMap : public GameMap
