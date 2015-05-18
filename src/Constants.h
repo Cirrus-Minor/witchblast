ee4d19e67cd33e48fbdb897fc0c50fcaa81880df
Seby <sebygames@gmail.com>
2015-05-18 20:09:23 +0200

    Update version number

--- src/Constants.h
+++ src/Constants.h
@@ -20,15 +20,15 @@
 #include <string>
 
 const std::string APP_NAME =      "Witch Blast";
-const std::string APP_VERSION =   "0.5.4";
+const std::string APP_VERSION =   "0.6";
 
 const std::string CONFIG_FILE =     "config.dat";
 const std::string SAVE_FILE =       "game.sav";
 const std::string SAVE_DATA_FILE =  "data/data.sav";
 const std::string HISCORES_FILE =   "data/scores.dat";
 
-const std::string SAVE_VERSION =    "SAVE_0.5.4";
-const std::string SCORE_VERSION =   "V054";
+const std::string SAVE_VERSION =    "SAVE_0.6";
+const std::string SCORE_VERSION =   "V060";
 
 const int NB_LANGUAGES = 5;
 const std::string languageString[NB_LANGUAGES] = { "english", "french", "german", "spanish", "russian" };
